21fdeca97158377b2b39f5d9e870d8ab2a01dbd2
jmcooper <jim@cooperplanet.com>
2018-04-30 22:08:05 -0600

    Create RobotArms.vue

---
+++ src/parts/RobotArms.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Arms</h2>
+    The arms are how your robot will interact with the world. They come in a variety of shapes and functions.
+    <div v-for="(arm, idx) in arms" :key="idx">
+      <h4>{{arm.title}}</h4>
+      <div>{{arm.description}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import parts from '../data/parts';
+
+export default {
+  name: 'RobotArms',
+  data() {
+    return { arms: parts.arms };
+  },
+};
+</script>
