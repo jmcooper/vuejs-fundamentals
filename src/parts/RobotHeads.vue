f24d00ce22d64325d589c77a82d96b83a2685b3e
jmcooper <jim@cooperplanet.com>
2018-04-30 22:12:49 -0600

    Create RobotHeads.vue

---
+++ src/parts/RobotHeads.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Heads</h2>
+    The head is where the brain of your robot will reside. Heads have
+    different capabilities so be sure to choose the one that fits your needs.
+    <div v-for="(head, idx) in heads" :key="idx">
+      <h4>{{head.title}}</h4>
+      <div>{{head.description}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import parts from '../data/parts';
+
+export default {
+  name: 'RobotHeads',
+  data() {
+    return { heads: parts.heads };
+  },
+};
+</script>
