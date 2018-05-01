b1a46434e3d699ad9e1c7ab512b4de73a8252346
jmcooper <jim@cooperplanet.com>
2018-04-30 22:13:04 -0600

    Create RobotTorsos.vue

---
+++ src/parts/RobotTorsos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Torsos</h2>
+    The torso is the central part of your robot that holds everything
+    together. Choosing the right torso will help ensure your robot
+    functions well with the parts you choose.
+    <div v-for="(torso, idx) in torsos" :key="idx">
+      <h4>{{torso.title}}</h4>
+      <div>{{torso.description}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import parts from '../data/parts';
+
+export default {
+  name: 'RobotTorsos',
+  data() {
+    return { torsos: parts.torsos };
+  },
+};
+</script>
