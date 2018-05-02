10560f070c6332bfff8b5a870d4a143bf6639f0b
jmcooper <jim@cooperplanet.com>
2018-05-02 17:38:09 -0600

    Create ShoppingCart.vue

---
+++ src/cart/ShoppingCart.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Cart</h1>
+    <table>
+      <thead>
+        <tr>
+          <th class="robot-title">
+            Robot
+          </th>
+          <th class="cost">
+            Cost
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(robot, index) in cart" :key="index">
+          <td class="robot-title">
+            {{robot.head.title}}
+          </td>
+          <td class="cost">
+            {{robot.cost}}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Cart',
+};
+</script>
+
+<style scoped>
+  td, th {
+    padding: 5px;
+  }
+  .robot-title {
+    text-align: left;
+    padding-right: 200px;
+  }
+  .cost {
+    text-align: right;
+  }
+</style>
