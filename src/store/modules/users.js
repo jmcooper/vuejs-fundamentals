079e9f45bec6c87fe6ea81dc02cf965a6ef3aca1
jmcooper <jim@cooperplanet.com>
2018-05-05 08:21:12 -0600

    Create users.js

---
+++ src/store/modules/users.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+export default {
+  state: {
+    user: null
+  },
+  mutations: {
+    updateCurrentUser (state, user) {
+      state.user = user
+    }
+  },
+  getters: {
+  },
+  actions: {
+    signIn ({commit}) {
+      axios.post('/api/sign-in')
+        .then(result => commit('updateCurrentUser', result.data))
+        .catch(console.error)
+    }
+  }
+}
